64a6acda23098bbfaa7a2adf2b49456832605a59
Jace7Valentine <mitchkm12@gmail.com>
2017-06-22 19:37:08 +0900

    Randomly generated build orders and evaluated them. Check the data in ./R_code/Examples/random_run1

--- R_Code/Genetic_Algorithm_BO.R
+++ R_Code/Genetic_Algorithm_BO.R
@@ -1,9 +1,43 @@
+library(GA)
+library(stringr)
+setwd("C:/Users/mitch/Documents/SCBW_AI/R_Code")
+
+# # Altered GA package functions
+# gaperm_Population <- function(object, size=0, r=FALSE,...)
+# {
+#   # Generate popSize random permutations in the range [min, max]
+#   int <- seq.int(object@min, object@max)
+#   n <- if (size > 0) size else length(int)
+#   population <- matrix(NA, nrow = object@popSize, ncol = n)
+#   for(i in 1:object@popSize)
+#     population[i,] <- if(size > 0 && r)  sample.int(object@max, size, replace = r) else sample(int, replace=F)
+#   return(population)
+# }
+
+# Altered GA package function to create a population with varied number of parameters per individual
+gaperm_Population_varied <- function(object, size=c(1, 1, 1), r=FALSE,...)
+{
+  if (length(size) != object@popSize) return(NULL)
+  int <- seq.int(object@min, object@max)
+  # Generate popSize random permutations with varied lenght in the range [min, max]
+  population <- matrix(list(), nrow = object@popSize, ncol = 1)
+  for(i in 1:object@popSize)
+    population[[i]] <- sample(int, size[i], replace = r)
+  return(population)
+}
+
+
 # Helper Functions and Variables: 
 result_Headers <- "Win, Total_Minerals, Total_Gas, Custom_score, Unit_Score, Kill_Score, Building_Score, Razing_Score, Elapsed_Time"
 indvInfo <- c(1,2,3,4)
+VAL_NAMES <- c("SCV", "Marine", "Supply_Depot", "Barracks", "Push_Marines")
 
 report <- function(...) {
   writeLines(paste(..., sep = ""))
+}
+
+toBOString <- function(bo) {
+  return(paste(length(bo), ", ", toString(bo), sep=""))
 }
 
 createID <- function(gen, indv, game) {
@@ -12,6 +46,25 @@ createID <- function(gen, indv, game) {
               "_Game", str_pad(game, 2, side = "left", pad = "0"),
               sep = "")
   returnValue(id)
+}
+
+pad <- function(x, fill = NA) {
+  max_length <- max(lengths(x))
+  sapply(x, function(y){
+    c(y, rep(fill, max_length - length(y)))
+  })
+  
+}
+
+numToBO <- function(y) {
+  for(v in 1:length(VAL_NAMES)) {
+    y[y==(v-1)] <- VAL_NAMES[v]
+  }
+  return(y)
+}
+
+meaningfulBO <- function(x) {
+  sapply(x, numToBO)
 }
 
 # Simulates a single game of starcraft and returns the resulting scores of that game
@@ -27,7 +80,7 @@ simGame <- function(ID, BO_Indv) {
     #Send Build Order 
     writeLines(BO_Indv, con)
     
-    report(ID, ": Recieved build order...")
+    report(ID, ": Sending build order...")
     msg <- readLines(con, 1, skipNul = T)
     report(ID, "_AI: ", msg)
     
@@ -35,7 +88,7 @@ simGame <- function(ID, BO_Indv) {
     writeLines(ID, con)
     
     # get results & convert to table
-    report(ID, ": Waiting for end of game...")
+    report(ID, ": Waiting for end of game(results)...")
     result <- readLines(con, 1, skipNul = T)
     
     txtCon <- textConnection(paste(result_Headers,"\n", result))
@@ -53,14 +106,23 @@ simGame <- function(ID, BO_Indv) {
     returnValue(scores)
 }
 
-### Config ###
-MAX_GEN <- 50
-POP_SIZE <- 10
-GAMES_PER_INDV <- 5
+####### Config #######
+MAX_GEN <- 1
+POP_SIZE <- 100
+GAMES_PER_INDV <- 1
+MAX_INSTR <- 4
+MIN_ORDERS <- 70
+MAX_ORDERS <- 400
+
+# Build GA-package ga class object
+ga <- new('ga')
+ga@min <- 0
+ga@max <- MAX_INSTR
+ga@popSize <- POP_SIZE
 
 # etc.
 
-##############
+####################
 
 main <- function() {
   gen <<- 1
@@ -83,13 +145,13 @@ main <- function() {
                          Unit_Score=NA,
                          Kill_Score=NA,
                          Building_Score=NA,
-                         RAzing_Score=NA,
+                         Razing_Score=NA,
                          Elapsed_Time=NA)
   
-  return()
-  temporary <- c(5, 0, 0, 0, 2, 3)
+  # Generate first generation individuals
+  builds[1,] <<- gaperm_Population_varied(ga, size=sample(c(MIN_ORDERS:MAX_ORDERS), ga@popSize, replace=T), r=T)
   
-  # TODO: generate first generation individuals
+  # return(ga@popSize)
   
   # Full run of the Genetic Algorithm
   while(gen <= MAX_GEN) {
@@ -101,7 +163,7 @@ main <- function() {
         run_data[run_data.row, +indvInfo] <<- c(gen, indv, game, NA)
         
         run_data[run_data.row, -indvInfo] <<- simGame(ID=createID(gen, indv, game),
-                                                      BO_Indv=toString(temporary))
+                                                      BO_Indv=toBOString(builds[[gen,indv]]))
         
         run_data.row <<- run_data.row + 1
         game <<- game + 1
